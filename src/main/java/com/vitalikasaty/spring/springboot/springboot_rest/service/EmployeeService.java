dc395f895d029c32ccfd2d6763210865975a7c4c
Vitali Kasaty <vitalik007007@gmail.com>
2022-06-26 21:17:00 +0300

    Create project based on a previous project

---
+++ src/main/java/com/vitalikasaty/spring/springboot/springboot_rest/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.vitalikasaty.spring.springboot.springboot_rest.service;
+
+import java.util.List;
+
+import com.vitalikasaty.spring.springboot.springboot_rest.entity.Employee;
+
+public interface EmployeeService {
+	public List<Employee> getAllEmployees();
+	public void saveEmployee(Employee employee);
+	public Employee getEmployee(int id);
+	public void deleteEmployee(int id);
+}
