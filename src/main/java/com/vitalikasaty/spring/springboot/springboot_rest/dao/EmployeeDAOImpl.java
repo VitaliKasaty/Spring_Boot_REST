64761b003005ffcc654e42f480ee5bb24034ec78
Vitali Kasaty <vitalik007007@gmail.com>
2022-06-27 20:05:00 +0300

    Use JPA in EmployeeDAOImpl

--- src/main/java/com/vitalikasaty/spring/springboot/springboot_rest/dao/EmployeeDAOImpl.java
+++ src/main/java/com/vitalikasaty/spring/springboot/springboot_rest/dao/EmployeeDAOImpl.java
@@ -3,9 +3,10 @@ package com.vitalikasaty.spring.springboot.springboot_rest.dao;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
-import org.hibernate.Session;
-import org.hibernate.query.Query;
+//import org.hibernate.Session;
+//import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,8 +24,12 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 	@Transactional 
 	public List<Employee> getAllEmployees() {
 
-		Session session = entityManager.unwrap(Session.class);
-		List<Employee> allEmployees = session.createQuery("from Employee", Employee.class).getResultList();
+//		Session session = entityManager.unwrap(Session.class);
+//		List<Employee> allEmployees = session.createQuery("from Employee"
+//				, Employee.class).getResultList();
+		
+		Query query = entityManager.createQuery("from Employee"); //Использование JPA
+		List<Employee> allEmployees = query.getResultList();
 
 		return allEmployees;
 	}
@@ -32,15 +37,23 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 	@Override
 	public void saveEmployee(Employee employee) {	
 		
-		Session session = entityManager.unwrap(Session.class);		
-		session.saveOrUpdate(employee);		
+//		Session session = entityManager.unwrap(Session.class);		
+//		session.saveOrUpdate(employee);	
+		
+		Employee newEmployee = entityManager.merge(employee);
+		employee.setId(newEmployee.getId());
+		//По сути можно сохранить работника и одной строкой entityManager.merge(employee);
+		//Тк spring сам добавит ему корректный id
+		//Но делаем это только для себя, чтобы в респонсе видеть реальный id, а не равный 0.
 	}
 
 	@Override
 	public Employee getEmployee(int id) {
 		
-		Session session = entityManager.unwrap(Session.class);
-		Employee employee = session.get(Employee.class, id);
+//		Session session = entityManager.unwrap(Session.class);
+//		Employee employee = session.get(Employee.class, id);
+		
+		Employee employee = entityManager.find(Employee.class, id);
 		
 		return employee;
 	}
@@ -48,8 +61,13 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 	@Override
 	public void deleteEmployee(int id) {
 		
-		Session session = entityManager.unwrap(Session.class);
-		Query<Employee>	query = session.createQuery("delete from Employee where id=:employeeId");		
+//		Session session = entityManager.unwrap(Session.class);
+//		Query<Employee>	query = session.createQuery("delete from Employee where id=:employeeId");		
+//		query.setParameter("employeeId", id);
+//		query.executeUpdate();
+		
+		Query query = entityManager.createQuery("delete from Employee "
+				+ "where id=:employeeId");		
 		query.setParameter("employeeId", id);
 		query.executeUpdate();
 	}
